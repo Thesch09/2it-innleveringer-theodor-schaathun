fab78bbef82ec1de863fcb65308a9e84c6b5fcc8
Theodor Schaathun <thesch09@gvgs.no>
2026-09-08 19:01:45 +0200

    Fjernet validScore variabelen og la til poeng kategoriene og fikset en bug hvor selv om man satt inn f.eks 50 så ville man få at det ikke var godkjent

--- oppgaver/karakterer/main.py
+++ oppgaver/karakterer/main.py
@@ -1,13 +1,19 @@
 from rich import print
 
-validScore = False
 while True:
     poeng = input("Hvor mange poeng fikk du på oppgaven?\n")
     try:
         poeng = int(poeng)
-        if not poeng > 100 or not poeng < 0:
+        if poeng > 100 or poeng < 0:
             print("Det er enten mer enn 100 eller mindre enn 0. Velg et tall imellom")
         else:
-            validScore = True
+            if poeng < 50:
+               print("Dessverre har du ikke bestått")
+            elif poeng < 70:
+                print("Du har bestått")
+            elif poeng < 90:
+                print("Du har gjort det ganske så bra")
+            elif poeng >= 90:
+                print("Du har gjort det VELDIG bra")
     except:
         print("Det er ikke et tall. Vennligst skriv inn et tall")
