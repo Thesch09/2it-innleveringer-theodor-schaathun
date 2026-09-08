381595ffa4ffef5edbcae4db43067c8a2b0bdd6c
Theodor Schaathun <thesch09@gvgs.no>
2026-09-08 19:18:13 +0200

    Lagde butikk mappen med en main.py. Importerte print fra rich, lagde en dictonary for butikken og en for spilleren. Begynte på loopen

---
+++ oppgaver/butikk/main.py
@@ -0,0 +1,8 @@
+from rich import print # fin terminal
+
+storeStock = {"Energi Drikk":25, "Baguette":45}
+playerCart = {}
+
+while True:
+    print("Velkommen til Theo-butikken!!!")
+    print("Hva vil du gjøre?")
