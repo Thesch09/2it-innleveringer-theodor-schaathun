3a9aa0214848190dccee62755390d6a9e1be4e7b
Theodor Schaathun <thesch09@gvgs.no>
2026-09-09 17:43:33 +0200

    La til oversikt over handlekurven som viser hva som er i den og flyttet koden for å kjøpe ting til en funksjon

--- oppgaver/butikk/main.py
+++ oppgaver/butikk/main.py
@@ -4,6 +4,44 @@ import os
 storeStock = {"Energi Drikk":25, "Baguette":45}
 playerCart = {}
 
+def buyThing(storeStock, playerCart):
+    goodUnconfirmed = True
+    while goodUnconfirmed:
+        os.system("cls")
+        print("Varer\n")
+        for ware in storeStock:
+            print(f"{ware}: {storeStock[ware]}")
+        print("\nHva vil du kjøpe?")
+        purchasable = input().lower().split(" ")
+        newStr = ""
+        for word in purchasable:
+            word = word.capitalize()
+            newStr += f"{word} "
+        purchasable = newStr.strip()
+        if purchasable in storeStock:
+            while goodUnconfirmed:
+                print("Hvor mange vil kjøpe?")
+                amount = input()
+                try:
+                    amount = int(amount)
+                    goodUnconfirmed = False
+                    if purchasable in playerCart:
+                        playerCart.update({purchasable:amount+playerCart[purchasable]})
+                    else:
+                        playerCart.update({purchasable:amount})
+                    print(f"Kjøpte {amount} {purchasable}")
+                except ValueError:
+                    print("Det er ikke et helt tall, vennligst velg et helt tall")
+                    input()
+                    os.system("cls")
+
+def lookAtCart(storeStock, playerCart):
+    for item in playerCart:
+        print(f"{playerCart[item]}x {item}: {storeStock[item]*playerCart[item]}")
+    input()
+
+
+
 while True:
     os.system("cls")
     print("Velkommen til Theo-butikken!!!")
@@ -13,32 +51,6 @@ while True:
     print("3. Betale og DRA!!!")
     action = input()
     if action == "1":
-        goodUnconfirmed = True
-        while goodUnconfirmed:
-            os.system("cls")
-            print("Varer\n")
-            for ware in storeStock:
-                print(f"{ware}: {storeStock[ware]}")
-            print("\nHva vil du kjøpe?")
-            purchasable = input().lower().split(" ")
-            newStr = ""
-            for word in purchasable:
-                word = word.capitalize()
-                newStr += f"{word} "
-            purchasable = newStr.strip()
-            if purchasable in storeStock:
-                while goodUnconfirmed:
-                    print("Hvor mange vil kjøpe?")
-                    amount = input()
-                    try:
-                        amount = int(amount)
-                        goodUnconfirmed = False
-                        if purchasable in playerCart:
-                            playerCart.update({purchasable:amount+playerCart[purchasable]})
-                        else:
-                            playerCart.update({purchasable:amount})
-                        print(f"Kjøpte {amount} {purchasable}")
-                    except ValueError:
-                        print("Det er ikke et helt tall, vennligst velg et helt tall")
-                        input()
-                        os.system("cls")
+        buyThing(storeStock, playerCart)
+    elif action == "2":
+        lookAtCart(storeStock, playerCart)
