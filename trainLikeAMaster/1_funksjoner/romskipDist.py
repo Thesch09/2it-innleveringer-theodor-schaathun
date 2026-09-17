78dc13eabc1e051a1276a11acddc4183295e192e
Theodor Schaathun <thesch09@gvgs.no>
2026-09-17 14:29:44 +0200

    Gjorde underoppgave number 5

--- trainLikeAMaster/1_funksjoner/romskipDist.py
+++ trainLikeAMaster/1_funksjoner/romskipDist.py
@@ -2,8 +2,18 @@
 # The sirens call of the balatro music from balatro being open and actually playing it's music (pretty good music) fills me with fuel to finish these far too simple tasks. I'm using more brainpower writing these funny little messages than actually doing the tasks
 
 def calcTravel(speed, time):
-    return f"Romskipet har reist {speed * time}km\n"
+    dist = speed * time
+    print(f"Romskipet har reist {dist}km")
+    if dist < 0:
+        return "Du går bakveis\n"
+    elif dist < 5000:
+        return "Kort tur\n"
+    elif dist < 20000:
+        return "Middels tur\n"
+    else:
+        return "Lang tur\n"
 
-print(calcTravel(5000, 3))
 print(calcTravel(-3,999))
+print(calcTravel(10,90))
+print(calcTravel(5000, 3))
 print(calcTravel(10000, 10000))
