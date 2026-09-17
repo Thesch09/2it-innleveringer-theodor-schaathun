2546fca1bd128bfcec8d731f49fffa5eba5c51ec
Theodor Schaathun <thesch09@gvgs.no>
2026-09-17 14:23:33 +0200

    Gjorde reise underoppgaven

---
+++ trainLikeAMaster/1_funksjoner/romskipDist.py
@@ -0,0 +1,9 @@
+# Ingen AI/KI er brukt på denne oppgaven. Jeg, Theodor Schaathun, liker ikke å bruke det, jeg vil lære ting selv
+# The sirens call of the balatro music from balatro being open and actually playing it's music (pretty good music) fills me with fuel to finish these far too simple tasks. I'm using more brainpower writing these funny little messages than actually doing the tasks
+
+def calcTravel(speed, time):
+    return f"Romskipet har reist {speed * time}km\n"
+
+print(calcTravel(5000, 3))
+print(calcTravel(-3,999))
+print(calcTravel(10000, 10000))
