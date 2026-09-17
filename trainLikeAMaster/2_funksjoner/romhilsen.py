8c8f2ee96cc66a9899acaf6dd6d878c4d2aba6e4
Theodor Schaathun <thesch09@gvgs.no>
2026-09-17 13:42:59 +0200

    Gjorde romhilsen underoppgaven

---
+++ trainLikeAMaster/2_funksjoner/romhilsen.py
@@ -0,0 +1,8 @@
+# Ingen AI/KI er brukt på denne oppgaven. Jeg, Theodor Schaathun, liker ikke å bruke det, jeg vil lære ting selv
+# Og hvorfor hadde jeg brukt det, denne oppgaven er ENKEL
+
+def romhilsen():
+    return "Velkommen til romstasjonen!\nGjør deg klar for avreise."
+
+for i in range(3):
+    print(romhilsen())
