a6bb3578109c8aaca766d4b6c96ece8b664a20a8
Theodor Schaathun <thesch09@gvgs.no>
2026-09-25 13:40:31 +0200

    Gjorde underoppgave 2, who would've guessed?

--- trainLikeAMaster/3_listTuplets/main.py
+++ trainLikeAMaster/3_listTuplets/main.py
@@ -11,3 +11,12 @@ print(planets)
 print(planets[0])
 print(planets[-1])
 print(len(planets))
+
+planets.remove("Neptune")
+shatteredPlanet = planets.pop(2)
+print(f"Whoops! It looks like \"{shatteredPlanet}\" cracked, crumbled and shattered, and therefore is lost")
+if shatteredPlanet != "The Planet that is slowly Cracking and Crumbling and Shattering and will soon be Lost, girl":
+    print("\tWhat do you mean that \"The Planet that is slowly Cracking and Crumbling and Shattering and will soon be Lost, girl\" wasn't the one to crack, crumble and shatter, and is therefore not lost???")
+else:
+    print("\tWho would've guessed that \"The Planet that is slowly Cracking and Crumbling and Shattering and will soon be Lost, girl\" cracked, crumbled and shattered, and is therefore lost?")
+print(planets)
