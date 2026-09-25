8d769422e683f1e7c72980394d65610d03ce6556
Theodor Schaathun <thesch09@gvgs.no>
2026-09-25 13:33:14 +0200

    Gjorde underoppgave 1

---
+++ trainLikeAMaster/3_listTuplets/main.py
@@ -0,0 +1,13 @@
+# Null LLM basert AI/KI blir brukt av Theodor Schaathun
+from rich import print
+
+planets = ["Popstar", "New World", "The Planet that is slowly Cracking and Crumbling and Shattering and will soon be Lost, girl", "PNF 404", "Not Neptune"]
+print(planets)
+print(planets[0])
+print(planets[-1])
+print(len(planets))
+planets.append("Neptune")
+print(planets)
+print(planets[0])
+print(planets[-1])
+print(len(planets))
